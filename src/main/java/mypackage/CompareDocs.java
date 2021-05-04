c3276446f460bd93d843774a5f97c705a94fc077
worthless1 <imoodivance1@gmail.com>
2021-05-04 13:42:50 +0300

    DCF-72 Added third-party library for document comparison

--- src/main/java/mypackage/CompareDocs.java
+++ src/main/java/mypackage/CompareDocs.java
@@ -1,109 +1,53 @@
 package mypackage;
 
-import com.github.difflib.DiffUtils;
-import com.github.difflib.patch.AbstractDelta;
-import com.github.difflib.patch.Patch;
 import com.github.difflib.text.DiffRow;
 import com.github.difflib.text.DiffRowGenerator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class CompareDocs {
 
     private static final Logger logger = LogManager.getLogger(CompareDocs.class);
 
-    // Helper method for get the file content
-    private static List fileToLines(String filename) {
-        List lines = new LinkedList();
-        String line = "";
-        try {
-            BufferedReader in = new BufferedReader(new FileReader(filename));
-            while ((line = in.readLine()) != null) {
-                lines.add(line);
-            }
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-            logger.error(e);
+    public List<DiffRow> Comparison(File file1, File file2) throws IOException {
+        List<DiffRow> rows = null;
+        if (file1.equals(file2)) {
+            logger.error("The same file names were specified " + file1 + " and " + file2);
+            throw new FileNotFoundException("Comparison error: to compare files, you need to specify different file names.");
         }
-        return lines;
-    }
-
-    public TreeMap<Integer, String> comparison(String path1, String path2) {
-        TreeMap<Integer, String> differences = new TreeMap<>();
         logger.debug("Start reading files");
+        List<String> original = Files.readAllLines(file1.toPath());
+        List<String> revised = Files.readAllLines(file2.toPath());
 
-        try (BufferedReader reader1 = new BufferedReader(new FileReader(path1));
-             BufferedReader reader2 = new BufferedReader(new FileReader(path2))) {
-
-            String line1 = null;
-            String line2 = null;
-            int count = 0;
-            //reading lines from a stream
-            while ((line1 = reader1.readLine()) != null && (line2 = reader2.readLine()) != null) {
-                count++;
-                //adding lines to HashMap
-                if (!line1.equals(line2)) {
-                    differences.put(count, line2);
-                }
-            }
-            logger.info("Files were read successfully, number of lines: " + count);
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-            logger.error(e);
-        }
-        return differences;
+        DiffRowGenerator generator = DiffRowGenerator.create().build();
+        rows = generator.generateDiffRows(original, revised);
+        return rows;
     }
 
-    public void print(TreeMap<Integer, String> diff) {
+    public void print(List<DiffRow> rows) {
         //outputting modified lines
-        for (Map.Entry<Integer, String> entry : diff.entrySet()) {
-            System.out.printf("%d:<%s>\n", entry.getKey(), entry.getValue());
-
-        }
-        logger.debug("Lines printed successfully");
-
-    }
-
-    public void newComparison() throws IOException {
-        List<String> original = Files.readAllLines(new File("1.txt").toPath());
-        List<String> revised = Files.readAllLines(new File("2.txt").toPath());
-
-        Patch<String> patch = DiffUtils.diff(original, revised);
-
-        DiffRowGenerator generator = DiffRowGenerator.create()
-                .showInlineDiffs(false)
-                .mergeOriginalRevised(false)
-                .build();
-
-        List<DiffRow> rows = generator.generateDiffRows(original, revised);
-
-        System.out.println("Первый сравниватель:");
-        for (AbstractDelta delta : patch.getDeltas()) {
-            System.out.println(delta.getType() + " " + delta.getTarget());
-            System.out.println(delta);
-        }
         int count = 0;
-        System.out.println("\nВторой сравниватель:");
         for (DiffRow row : rows) {
             count++;
-            if (!row.getTag().equals(DiffRow.Tag.EQUAL)) {
-                //System.out.println(count + ": <" + row.getTag() + " " + row.getOldLine() + "> to <" + row.getNewLine() + ">");
-                System.out.printf("%s: %s <%s> to <%s>\n", count, row.getTag(), row.getOldLine(), row.getNewLine());
+            switch (row.getTag()) {
+                case CHANGE:
+                    System.out.printf("%s: CHANGE <%s> to <%s>\n", count, row.getOldLine(), row.getNewLine());
+                    break;
+                case DELETE:
+                    System.out.printf("%s: DELETE <%s>\n", count, row.getOldLine());
+                    break;
+                case INSERT:
+                    System.out.printf("%s: INSERT <%s>\n", count, row.getNewLine());
+                    break;
             }
         }
-
-
+        logger.debug("Lines printed successfully");
     }
-
 
 }
--- src/main/java/mypackage/Main.java
+++ src/main/java/mypackage/Main.java
@@ -1,40 +1,40 @@
 package mypackage;
 
-import com.github.difflib.patch.DiffException;
+import com.github.difflib.text.DiffRow;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) {
-//        TreeMap<Integer, String> diff = new TreeMap<>();
-//
-//    CompareDocs cd = new CompareDocs();
-//    //checking the number of arguments entered
-//        if (args.length < 2) {
-//        System.err.println("Specify filenames in program arguments!");
-//        logger.error("File names were not specified in program arguments");
-//        System.exit(0);
-//    }
-//    //checking for the existence of files
-//    File file1 = new File(args[0]);
-//    File file2 = new File(args[1]);
-//        if (file1.exists() && file2.exists()) {
-//        diff = cd.comparison(args[0], args[1]);
-//        cd.print(diff);
-//    } else {
-//        logger.error("File" + args[0] + " or " + args[1] + " was not found");
-//        System.err.printf("Check the spelling of the file name. %s or %s (The specified file cannot be found)", args[0], args[1]);
-//    }
+    public static void main(String[] args) throws IOException {
         CompareDocs cd = new CompareDocs();
-        try{
-            cd.newComparison();
-        }catch (IOException e){
-            System.out.println(e.getMessage());
+        List<DiffRow> rows;
+        //checking the number of arguments entered
+        if (args.length < 2) {
+            System.err.println("Specify filenames in program arguments!");
+            logger.error("File names were not specified in program arguments");
+            System.exit(0);
         }
-    }
+        //checking for the existence of files
+        File file1 = new File(args[0]);
+        File file2 = new File(args[1]);
+        if (file1.exists() && file2.exists()) {
+            try {
+                rows = cd.Comparison(file1, file2);
+                cd.print(rows);
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+                logger.error(e);
+            }
+        } else {
+            logger.error("File" + file1 + " or " + file2 + " was not found");
+            System.err.printf("Check the spelling of the file name. %s or %s (The specified file cannot be found)", args[0], args[1]);
+        }
 
+    }
 }
